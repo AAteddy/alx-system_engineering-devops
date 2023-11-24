c37232bc18a46a199274fc2dc289e46f9e024bdc
Tedros <tedsaasfaha@gmail.com>
2023-11-24 09:58:07 -0800

    0-create_a_file.pp file updated

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,10 +1,10 @@
-# create a file in /tmp.
+# Create a file 'school' in /tmp.
 
 file { 'school':
- ensure  =>  'present',
- mode  =>  '0744',
- owner  =>  'www-data',
- group  =>  'www-data',
- content  =>  'I love Puppet',
- path  =>  '/tmp/school',
+  ensure  => 'present',
+  content => 'I love Puppet',
+  group   => 'www-data',
+  mode    => '0744',
+  owner   => 'www-data',
+  path    => '/tmp/school',
 }
