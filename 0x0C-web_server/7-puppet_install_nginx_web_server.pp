dd9d5fd5c9abdd85cbe1ca86ede8beeea222d67e
Tedros <tedsaasfaha@gmail.com>
2023-11-29 07:57:08 -0800

    7-puppet_install_nginx_web_server.pp projecte file updated

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,22 +1,23 @@
 # Automating Nginx server configuration using Puppet.
 
 package { 'nginx':
-  ensure  => 'installed',
+  ensure => 'installed',
 }
 
-file_line { 'install':
-  ensure  => 'present',
-  path    => '/etc/nginx/sites-enabled/default',
-  after   => 'listen 80 default_server;',
-  line    => 'rewrite ^/redirect_me https://www.github.com/AAteddy permanent;',
-}
 
 file { '/var/www/html/index.html':
-  ensure  => 'Hello World!',
+  ensure  => 'file',
+  content => 'Hello World!',
+  mode    => '0644',
+  require => Package['nginx'],
+}
+
+exec { 'append_redirect_me':
+  command => "/usr/bin/sed -i '/^}$/i \\\n\tlocation \\/redirect_me {return 301 https:\\/\\/www.github.com\\/AAteddy;}' /etc/nginx/sites-available/default",
 }
 
 service { 'nginx':
-  ensure   => 'running',
-  enable   => true,
-  require  => Package['nginx'],
+  ensure  => 'running',
+  enable  => true,
+  require => Package['nginx'],
 }
