78cc3bf0f79ef266e83518077e7166d692905313
Tedros <tedsaasfaha@gmail.com>
2024-01-02 09:39:02 -0800

    3-dictionary_of_list_of_dictionaries.py project file implemented

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Exports to-do list information for a given employee ID to JSON format.
+"""
+
+import json
+import requests
+import sys
+
+
+if __name__ == '__main__':
+    url = 'https://jsonplaceholder.typicode.com/'
+    user = requests.get(url + 'users').json()
+    todos = requests.get(url + 'todos').json()
+
+    with open("todo_all_employees.json", "w", newline="") as json_dict:
+        for employee in user:
+            json.dump({employee.get('id'):
+                    [{"username": employee.get('username'), "task": td.get('title'), "completed": td.get('completed')} 
+                    for td in todos]}, json_dict)
