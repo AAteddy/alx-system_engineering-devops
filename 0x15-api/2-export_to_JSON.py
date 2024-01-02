a0671797500c631b8eae09b46b37a559255cd736
Tedros <tedsaasfaha@gmail.com>
2024-01-02 04:05:08 -0800

    2-export_to_JSON.py project file updated

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Exports to-do list information for a given employee ID to CSV format.
+"""Exports to-do list information for a given employee ID to JSON format.
 """
 
 import json
