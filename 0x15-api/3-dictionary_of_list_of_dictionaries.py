298dda0af1fb5f85bcbc9b7b13ea21c30dd5235f
Tedros <tedsaasfaha@gmail.com>
2024-01-02 11:46:23 -0800

    3-dictionary_of_list_of_dictionaries.py file updated for checker errors

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -6,15 +6,16 @@ import json
 import requests
 
 
-if __name__ == '__main__':
-    url = 'https://jsonplaceholder.typicode.com/'
-    user = requests.get(url + 'users').json()
-    todos = requests.get(url + 'todos').json()
+if __name__ == "__main__":
+    url = "https://jsonplaceholder.typicode.com/"
+    users = requests.get(url + "users").json()
 
-    with open("todo_all_employees.json", "w", newline="") as json_dict:
-        for employee in user:
-            json.dump({employee.get('id'):
-                    [{"username": employee.get('username'), 
-                        "task": td.get('title'), 
-                        "completed": td.get('completed')} 
-                    for td in todos]}, json_dict)
+    with open("todo_all_employees.json", "w") as jsonfile:
+        json.dump({
+            u.get("id"): [{
+                "task": t.get("title"),
+                "completed": t.get("completed"),
+                "username": u.get("username")
+            } for t in requests.get(url + "todos",
+                                    params={"userId": u.get("id")}).json()]
+            for u in users}, jsonfile)
