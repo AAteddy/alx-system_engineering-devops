346b53405f7cbb6744d9e3e44e2f32d4ccefae8b
Tedros <tedsaasfaha@gmail.com>
2024-01-02 10:00:06 -0800

    0-gather_data_from_an_API.py project file updated by correcting output

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,17 +1,14 @@
 #!/usr/bin/python3
-"""Accessing a REST API's url and returns todo-list information
-for a given employee id.
-"""
-
+"""Returns to-do list information for a given employee ID."""
 import requests
 import sys
 
-if __name__ == '__main__':
-    url = 'https://jsonplaceholder.typicode.com/'
-    user = requests.get(url + 'users/{}'.format(sys.argv[1])).json()
-    todos = requests.get(url + 'todos', params={'userId': sys.argv[1]}).json()
+if __name__ == "__main__":
+    url = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(url + "users/{}".format(sys.argv[1])).json()
+    todos = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
 
-    completed = [td.get('title') for td in todos if td.get('completed') is True]
-    print("Employee {} is done with tasks({}/{}):"
-            .format(user.get('name'), len(completed), len(todos)))
+    completed = [td.get("title") for td in todos if td.get("completed") is True]
+    print("Employee {} is done with tasks({}/{}):".format(
+        user.get("name"), len(completed), len(todos)))
     [print("\t {}".format(c)) for c in completed]
