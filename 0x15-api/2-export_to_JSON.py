a6613a2eab72b72aab019a6e49af0104f9b444f0
Tedros <tedsaasfaha@gmail.com>
2024-01-02 04:04:18 -0800

    2-export_to_JSON.py project file implemented

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Exports to-do list information for a given employee ID to CSV format.
+"""
+
+import json
+import requests
+import sys
+
+
+if __name__ == '__main__':
+    url = 'https://jsonplaceholder.typicode.com/'
+    user = requests.get(url + 'users/{}'.format(sys.argv[1])).json()
+    todos = requests.get(url + 'todos', params={'userId': sys.argv[1]}).json()
+
+    with open("{}.json".format(sys.argv[1]), "w", newline="") as json_file:
+        json.dump({sys.argv[1]: 
+            [{"task": td.get('title'), "completed": td.get('completed'), "username": user.get('username')} 
+                for td in todos]}, json_file)
