30138aa80b7cada4d143699b0bd4729d908c1246
Tedros <tedsaasfaha@gmail.com>
2024-01-18 12:05:54 -0800

    0-subs.py file code updated implemented

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -4,14 +4,13 @@ number of subscribers on a given subreddit.
 """
 import requests
 
+headers = { "User-Agent": "CustomUserAgent/1.0" }
 
 def number_of_subscribers(subreddit):
     """Returns the total number of subscribers on a given subreddit.
     OR - Returns 0 if not a valid subreddit.
     """
-    url = "https://www.reddit.com/r/{}/about.json()".format(subreddit)
-    headers = { "User-Agent": "CustomUserAgent/1.0" }
-
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 200:
         result = response.json()
