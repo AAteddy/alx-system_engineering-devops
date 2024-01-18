9f39656c89fb49796e73ad30419c92bc968b9331
Tedros <tedsaasfaha@gmail.com>
2024-01-18 15:58:58 -0800

    1-top_ten.py project file implemented

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""A function that queries the Reddit API and returns 
+the titles of the first 10 hot posts listed.
+"""
+import requests
+
+headers = { "User-Agent": "CustomUserAgent/1.0" }
+
+def top_ten(subreddit):
+    """Returns the titles of the first 10 hot posts listed
+    for a given subreddit.
+    OR - Returns 0 if not a valid subreddit.
+    """
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    params = { "limit": 10 }
+    response = requests.get(url, headers=headers, params=params, allow_redirects=False)
+    if response.status_code == 200:
+        result = response.json()
+        for post in result["data"]["children"]:
+            print(post["data"]["title"])
+    else:
+        print("None")
