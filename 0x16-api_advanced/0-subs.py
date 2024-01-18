7da51932a66d99bee8d07d9f334c40b23252ed68
Tedros <tedsaasfaha@gmail.com>
2024-01-18 12:03:14 -0800

    0-subs.py file code updated implemented

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -9,11 +9,12 @@ def number_of_subscribers(subreddit):
     """Returns the total number of subscribers on a given subreddit.
     OR - Returns 0 if not a valid subreddit.
     """
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = { "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)" }
+    url = "https://www.reddit.com/r/{}/about.json()".format(subreddit)
+    headers = { "User-Agent": "CustomUserAgent/1.0" }
 
     response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 404:
+    if response.status_code == 200:
+        result = response.json()
+        return result["data"]["subscribers"]
+    else:
         return 0
-    result = response.json().get("data")
-    return result.get("subscribers")
