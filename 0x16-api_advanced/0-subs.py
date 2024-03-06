1bb429c27a0fff5d28780795a6cc2598f205e425
AAteddy <tedsaasfaha@gmail.com>
2024-03-06 01:33:33 -0800

    0-subs.py file correction implemented

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""A function that queries the Reddit API and returns the 
+"""
+A function that queries the Reddit API and returns the 
 number of subscribers on a given subreddit.
 """
 
@@ -7,19 +8,26 @@ import requests
 
 
 def number_of_subscribers(subreddit):
-    """Returns the total number of subscribers on a
+    """
+    Returns the total number of subscribers on a
     given subreddit.
     OR - Returns 0 if not a valid subreddit.
     """
+    
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {
+            'User-Agent': 'CustomUserAgent'
+            }
+    response = requests.get(url, headers=headers, allow_redirects=False)
 
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {"User-Agent": "CustomUserAgent/1.0"}
+    result = response
 
-    response = requests.get(url,
-                            headers=headers, 
-                            allow_redirects=False)
-    if response.status_code == 200:
-        results = response.json().get("data")
-        return results.get("subscribers")
-    else:
+    try:
+        # grab the info about the users' tasks
+        data = result.get('data')
+        subscribers = data.get('subscribers')
+    except:
         return 0
+    if subscribers is None:
+        return 0
+    return int(subscribers)
